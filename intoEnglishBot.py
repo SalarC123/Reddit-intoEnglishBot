216dc6c3aa073fd0c3a5de864ba110823b75a4f3
Tabinda <Tabinda@chaudrys-iMac.local>
2020-08-13 20:28:28 -0500

    Add in-depth comments to reduce confusion

--- intoEnglishBot.py
+++ intoEnglishBot.py
@@ -15,22 +15,30 @@ reddit = praw.Reddit(client_id= PUT_CLIENT_ID_HERE,
 # ---- intoEnglishBot ----
 
 def intoEnglishBot():
+    # Goslate must be initialized for later use of its langauge list
     translator = goslate.Goslate()
+    # Checks each comment posted to reddit
+    # but skips all comments made before the code runs
     for comment in reddit.subreddit('all').stream.comments(skip_existing=True):
-        translator = goslate.Goslate()
         try:
+            # bot looks for any comment that contains !intoEnglishBot
             botcall = re.compile('!intoEnglishBot',re.I).search(comment.body).group()
         except AttributeError:
             continue
 
         try:
+            # Gets the body of the comment above the comment with the bot call
             parentcomment = str(reddit.comment(reddit.comment(id=comment.id).parent().id).body)
         except:
-            comment.reply('There is no parent comment for me to tranlate!\n\nMake sure to reply to a comment next time or contact the creator "RedditGood123" if there are any issues.')
+            # Bot still replies when comment has no parent
+            comment.reply('There is no parent comment for me to tranlate!\n\nMake sure to reply to a comment next time or contact the creator "1Test2Bot3" if there are any issues.')
             continue
 
         b = TextBlob(parentcomment)
+        # Language codes are returned by detect_language()
+        # For example, 'es' is the code for Spanish
         languagecode = b.detect_language()
+        # Indexes dictionary with languagecodes and language names
         language = translator.get_languages()[str(languagecode)]
         try:
             translation = b.translate(to='en')
